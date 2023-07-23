b43235f6f0d914983d58d805514d53819f31fa11
RaziaAkbari <akbariharis14@gmail.com>
2023-07-23 20:58:51 +0430

    the 4th project was created

---
+++ 4th-project/index.js
@@ -0,0 +1,23 @@
+window.addEventListener("resize", toggle)
+const items = document.querySelector(".items")
+const icon = document.getElementById("icon")
+const dropdown = document.querySelector(".dropdown")
+
+function toggle() {
+  let parent = items.parentNode
+  if (window.innerWidth < 600) {
+    items.classList.add("hide")
+    icon.classList.remove("hide")
+    parent.appendChild(icon)
+  } else if (window.innerWidth > 600) {
+    console.log("here")
+    icon.classList.add("hide")
+    parent.removeChild(icon)
+
+    items.classList.remove("hide")
+  }
+}
+icon.addEventListener("click", () => {
+  dropdown.classList.toggle("hide")
+})
+toggle()
